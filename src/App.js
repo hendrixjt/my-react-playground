b82f9a3389b9fd369686917fccb0b33b5ae28f39
Joshua Hendrix <joshuathendrix@gmail.com>
2018-10-15 13:48:02 -0600

    added a switch button

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Joshua', age: 36 },
+      { name: 'Eden', age: 25 },
+      { name: 'AT', age: 29 }
+    ],
+    otherState: 'some other thing'
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked');
+    // DON'T DO THIS this.state.persons[0].name = "Joshuas";
+    this.setState( {
+      persons: [
+      { name: 'Joshua', age: 36 },
+      { name: 'Eden', age: 25 },
+      { name: 'Anderson', age: 28 }
+      ] 
+    })
+  }
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
        <p>This is really Working!</p>
-       <Person name="Joshua" age="35" />
-       <Person name= "Eden" age="25" >My Hobbies: Basketball</Person>
-       <Person name="AT" age="29" />
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name= {this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name= {this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Basketball</Person>
+       <Person name= {this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
